425209aee6c080e7af5d94ae80a8ff02056c2a12
wazsmwazsm <18748406022@163.com>
2018-04-03 20:03:50 +0800

    461Hamming-Distance

---
+++ 461Hamming-Distance.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+
+    x, y := 3, 1
+    
+    h := hammingDistance(x, y)
+    fmt.Println(h)
+}
+
+func hammingDistance(x int, y int) int {
+
+    count := 0
+    xor := x ^ y // 异或 获得一个不同位置疑 1 的数
+
+    for bitMask := 1; bitMask > 0; bitMask <<= 1 {
+		// 使用 bitMask 移位来比较每一位是 1 还是 0
+        if bitMask & xor != 0 {
+           count++ 
+        }
+    }
+    
+    return count
+}
+
