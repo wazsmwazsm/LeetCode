0e2a4866c05b84d6f1ded88a657111c917499895
wazsmwazsm <18748406022@163.com>
2018-07-30 11:49:44 +0800

    771JewelsandStones

---
+++ 771JewelsandStones.go
@@ -0,0 +1,63 @@
+/*
+
+
+You're given strings J representing the types of stones that are jewels, and S representing the stones you have.  Each character in S is a type of stone you have.  You want to know how many of the stones you have are also jewels.
+
+The letters in J are guaranteed distinct, and all characters in J and S are letters. Letters are case sensitive, so "a" is considered a different type of stone from "A".
+
+Example 1:
+
+Input: J = "aA", S = "aAAbbbb"
+Output: 3
+Example 2:
+
+Input: J = "z", S = "ZZ"
+Output: 0
+Note:
+
+S and J will consist of letters and have length at most 50.
+The characters in J are distinct.
+
+
+*/
+
+package main
+
+/* use strings */
+// import (
+//     "fmt"
+//     "strings"
+// )
+
+// func numJewelsInStones(J string, S string) int {
+//     count := 0;
+//     for _, v := range J {
+//         count += strings.Count(S, string(v))
+//     }
+//     return count;
+// }
+
+/* use map */
+import "fmt"
+
+func numJewelsInStones(J string, S string) int {
+	m := make(map[rune]bool)
+	for _, v := range J {
+        m[v] = true
+    }
+    count := 0;
+    for _, v := range S {
+        if m[v] {
+			count ++
+		}
+    }
+    return count;
+}
+
+
+func main() {
+    S := "aaAsssfasBA";
+    J := "aA";
+    num := numJewelsInStones(J, S)
+    fmt.Println(num);
+}
