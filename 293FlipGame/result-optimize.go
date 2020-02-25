9c9956ce54bacb2940c67379d89aa7f693351401
wazsmwazsm <942443360@qq.com>
2020-02-25 20:43:58 +0800

    add 239

---
+++ 293FlipGame/result-optimize.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+var tests = []string{
+	"+++--++-",
+	"++++-++-+--+-++-",
+	"++++",
+	"--",
+	"-+-++",
+}
+
+func main() {
+	for _, test := range tests {
+		result := generatePossibleNextMoves(test)
+		fmt.Println(result)
+	}
+}
+
+func generatePossibleNextMoves(s string) []string {
+	result := []string{}
+	var buf strings.Builder
+	for i := 0; i < len(s)-1; i++ {
+		if s[i] == '+' && s[i+1] == '+' {
+			buf.WriteString(s[0:i])
+			buf.WriteString("--")
+			buf.WriteString(s[i+2:])
+			result = append(result, buf.String())
+			buf.Reset()
+		}
+	}
+
+	return result
+}
---
+++ 293FlipGame/result.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+)
+
+var tests = []string{
+	"+++--++-",
+	"++++-++-+--+-++-",
+	"++++",
+	"--",
+	"-+-++",
+}
+
+func main() {
+	for _, test := range tests {
+		result := generatePossibleNextMoves(test)
+		fmt.Println(result)
+	}
+}
+
+func generatePossibleNextMoves(s string) []string {
+	result := []string{}
+
+	for i := 0; i < len(s)-1; i++ {
+		tmp := []byte(s)
+		if s[i] == '+' && s[i+1] == '+' {
+			tmp[i] = '-'
+			tmp[i+1] = '-'
+			result = append(result, string(tmp))
+		}
+	}
+
+	return result
+}
